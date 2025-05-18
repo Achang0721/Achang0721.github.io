8e3e8105d74cb5fd92df58ade8202c4210c353b2
Achang0721 <achang0721@gmail.com>
2025-05-18 21:46:49 +0800

    Site updated: 2025-05-18 21:46:39

--- js/fomal.js
+++ js/fomal.js
@@ -2819,8 +2819,8 @@ function createtime() {
   let currentTimeHtml = "";
   (currentTimeHtml =
     hnum < 18 && hnum >= 9
-      ? `<img class='boardsign' src='https://achang0721.github.io/assets/昌想屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~' style="width:10%"><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`
-      : `<img class='boardsign' src='https://achang0721.github.io/assets/昌想屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~' style="width:10%"><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`),
+      ? `<img class='boardsign' src='https://achang0721.github.io/assets/昌想屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~' style="width:20%"><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`
+      : `<img class='boardsign' src='https://achang0721.github.io/assets/昌想屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~' style="width:20%"><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`),
     document.getElementById("workboard") &&
     (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
